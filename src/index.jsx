e3b71256c8b4c5fe3cebdd79a54220178343bff3
Mr-Macko <max.allesch@gmx.de>
2021-12-29 01:03:21 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,17 +6,17 @@ import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-    render() {
-        return (
-            <div className='my-flix'>
-                <div>Good morning</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
 }
 
-// Find the root the app
+// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication),container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
