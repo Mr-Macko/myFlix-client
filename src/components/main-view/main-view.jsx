34c0e048e6defca0e3772d3117dde86d80444949
Mr-Macko <max.allesch@gmx.de>
2022-01-04 19:57:26 +0100

    changed render method

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -15,14 +15,18 @@ export class MainView extends React.Component {
     }
   }
 
-    render () {
-        return (
-            <div className='main-view'>
-                <div>Inception</div>
-                <div>The Shawshank Redemption</div>
-                <div>Gladiator</div>
-            </div>
-        );
+    render() {
+      const { movies, selectedMovie } = this.state;
+    
+      if (selectedMovie) return <MovieView movie={selectedMovie} />;
+    
+      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    
+      return (
+        <div className="main-view">
+          {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
+        </div>
+      );
     }
 }
 
