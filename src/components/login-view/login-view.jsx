cc52f525f5ef737123ebac3b728e3f1b519bed6d
Mr-Macko <max.allesch@gmx.de>
2022-01-07 15:51:39 +0100

    changed form styling

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -19,26 +19,23 @@ export function LoginView(props) {
     console.log('handleClickRegister');
     props.toRegistrationView('');
   }
-
+  
   return (
-    <div>
-      <form>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-      </form>
-      <div>
-        <span>Not an account yet?? </span>
-        <button type="submit" onClick={handleClickRegister}>Register</button>
-      </div>
-    </div>
-  )
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 // prop-types
