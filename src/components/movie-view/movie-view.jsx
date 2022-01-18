30cdd0d08112ae99cdfacca1ef135f6fc55e79a9
Mr-Macko <max.allesch@gmx.de>
2022-01-18 23:55:23 +0100

    added links to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,6 +8,8 @@ import './movie-view.scss'
 // React-Bootstrap components
 import { Button } from 'react-bootstrap';
 
+import { Link } from 'react-router-dom';
+
 export class MovieView extends React.Component {
 
   keypressCallback(event) {
@@ -41,10 +43,16 @@ export class MovieView extends React.Component {
         <div className="movie-genre">
           <span className="label">Genre: </span>
           <span className="value">{movie.Genre.Name}</span>
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">Genre</Button>
+          </Link>
         </div>
         <div className="movie-director">
           <span className="label">Director: </span>
           <span className="value">{movie.Director.Name}</span>
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button variant="link">Director</Button>
+          </Link>
         </div>
         <Button className='button' onClick={() => { onBackClick(null); }}>Back</Button>
 
