e6a2ff45b6422272ba1e04e14a383484d9f32357
Mr-Macko <max.allesch@gmx.de>
2022-01-19 23:31:33 +0100

    added render method

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,1 +1,26 @@
+import React from "react";
 import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+export class GenreView extends React.Component {
+    render() {
+        const { genre } = this.props;
+
+        return (
+            <div className="genre-view">
+                <div className="genre-name">
+                    <span className="label">Name: </span>
+                    <span className="value">{genre.Name}</span>
+                </div>
+                <div className="genre-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{genre.Description}</span>
+                </div>
+            </div>
+            <Link to={`/`}>
+                <Button>Return to Movies</Button>
+            </Link>
+        )
+    }
+}
