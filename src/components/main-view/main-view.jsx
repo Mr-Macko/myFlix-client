2b72c8f5e8f4ac242c26628780a586370243715a
Mr-Macko <max.allesch@gmx.de>
2022-01-19 22:28:40 +0100

    formated code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,35 +13,35 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 
 // react bootstrap
-import { Container, Navbar, NavbarBrand, Row, Nav, Col, Form, FormControl, Button} from "react-bootstrap";
+import { Container, Navbar, NavbarBrand, Row, Nav, Col, Form, FormControl, Button } from "react-bootstrap";
 
 export class MainView extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       movies: [],
       user: null,
-      selectedMovies: null
+      // selectedMovies: null
     };
   }
 
   getMovies(token) {
     axios.get('https://max-movie-api.herokuapp.com/movies', {
-      headers: { Authorization:`Bearer ${token}`}
+      headers: { Authorization: `Bearer ${token}` }
     })
-    .then(response => {
-      // Assign the result to the state
-      this.setState({
-        movies: response.data
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://max-movie-api.herokuapp.com/movies')
       .then(response => {
         this.setState({
@@ -58,18 +58,11 @@ export class MainView extends React.Component {
     this.setState({
       user: authData.user.Username
     });
-  
+
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.Username);
     this.getMovies(authData.token);
   }
-    // NOT NEEDED ANYMORE
-    // setSelectedMovie(newSelectedMovie) {
-    //   this.setState({
-    //     selectedMovie: newSelectedMovie
-    //   });
-    // }
-
   // NOT NEEDED ANYMORE
   // setSelectedMovie(newSelectedMovie) {
   //   this.setState({
@@ -95,7 +88,6 @@ export class MainView extends React.Component {
                 <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
               </Col>
               if (movies.length === 0) return <div className="main-view" />;
-              
               return movies.map(m => (
                 <Col md={3} key={m._id}>
                   <MovieCard movie={m} />
@@ -138,62 +130,62 @@ export class MainView extends React.Component {
             }
             } />
           </Routes>
-          </Row>
-        </BrowserRouter>
-      );
-    }
+        </Row>
+      </BrowserRouter>
+    );
+  }
 
-    // NOT NEEDED ANYMORE -> OLD RENDER METHOD
-    // render() {
-    //   const { movies, selectedMovie } = this.state;
-  
-    //   if (movies.length === 0) return <div className="main-view" />;
-  
-    //   return (
-    //     <div className="main-view">
+  // NOT NEEDED ANYMORE -> OLD RENDER METHOD
+  // render() {
+  //   const { movies, selectedMovie } = this.state;
 
-    //       <Navbar expand='lg' className='Navbar'>
-    //         <Container>
-    //           <NavbarBrand href='#home'>MyFlix</NavbarBrand>
-    //           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    //           <Navbar.Collapse id="responsive-navbar-nav">
-    //             <Nav className="me-auto">
-    //               <Nav.Link href="#profile">Profile</Nav.Link>
-    //               <Nav.Link href="#logout">Logout</Nav.Link>
-    //             </Nav>
-    //           </Navbar.Collapse>
-    //           <Form className="d-flex">
-    //               <FormControl
-    //                 type="search"
-    //                 placeholder="Search Movie"
-    //                 className="me-2"
-    //                 aria-label="Search"
-    //               />
-    //              <Button className='button'>Search</Button>
-    //             </Form>
-    //         </Container>
-    //       </Navbar>
+  //   if (movies.length === 0) return <div className="main-view" />;
 
-    //       {selectedMovie
-    //         ? (
-    //           <Row className='justify-content-md-center'>
-    //             <Col md={8}>
-    //               <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-    //             </Col>
-    //           </Row>
-    //           )
-    //         : (
-    //           <Row className="justify-content-md-center">
-    //             <Col md={3}>
-    //             {movies.map(movie => (
-    //               <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-    //             ))}
-    //             </Col>
-    //           </Row>
-    //         )
-    //       }
-    //     </div>
-    //   );
-    // }
+  //   return (
+  //     <div className="main-view">
+
+  //       <Navbar expand='lg' className='Navbar'>
+  //         <Container>
+  //           <NavbarBrand href='#home'>MyFlix</NavbarBrand>
+  //           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  //           <Navbar.Collapse id="responsive-navbar-nav">
+  //             <Nav className="me-auto">
+  //               <Nav.Link href="#profile">Profile</Nav.Link>
+  //               <Nav.Link href="#logout">Logout</Nav.Link>
+  //             </Nav>
+  //           </Navbar.Collapse>
+  //           <Form className="d-flex">
+  //               <FormControl
+  //                 type="search"
+  //                 placeholder="Search Movie"
+  //                 className="me-2"
+  //                 aria-label="Search"
+  //               />
+  //              <Button className='button'>Search</Button>
+  //             </Form>
+  //         </Container>
+  //       </Navbar>
+
+  //       {selectedMovie
+  //         ? (
+  //           <Row className='justify-content-md-center'>
+  //             <Col md={8}>
+  //               <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+  //             </Col>
+  //           </Row>
+  //           )
+  //         : (
+  //           <Row className="justify-content-md-center">
+  //             <Col md={3}>
+  //             {movies.map(movie => (
+  //               <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+  //             ))}
+  //             </Col>
+  //           </Row>
+  //         )
+  //       }
+  //     </div>
+  //   );
+  // }
 }
 
