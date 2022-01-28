eb0d999b063b13c853790388098052ebd5d4e809
Mr-Macko <max.allesch@gmx.de>
2022-01-28 18:48:56 +0100

    imported navbar to mainview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,6 +13,7 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from "../genre-view/genre-view";
+import { NavBar } from "../navbar/navbar";
 
 // react bootstrap
 import { Container, Navbar, NavbarBrand, Row, Nav, Col, Form, FormControl, Button } from "react-bootstrap";
@@ -92,6 +93,7 @@ export class MainView extends React.Component {
 
     return (
       <BrowserRouter>
+        <NavBar />
         <Row className="main-view justify-content-md-center">
             <Route exact path="/" render={() => {
               if (!user) return <Col>
@@ -143,58 +145,5 @@ export class MainView extends React.Component {
       </BrowserRouter>
     );
   }
-
-  // NOT NEEDED ANYMORE -> OLD RENDER METHOD
-  // render() {
-  //   const { movies, selectedMovie } = this.state;
-
-  //   if (movies.length === 0) return <div className="main-view" />;
-
-  //   return (
-  //     <div className="main-view">
-
-  //       <Navbar expand='lg' className='Navbar'>
-  //         <Container>
-  //           <NavbarBrand href='#home'>MyFlix</NavbarBrand>
-  //           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  //           <Navbar.Collapse id="responsive-navbar-nav">
-  //             <Nav className="me-auto">
-  //               <Nav.Link href="#profile">Profile</Nav.Link>
-  //               <Nav.Link href="#logout">Logout</Nav.Link>
-  //             </Nav>
-  //           </Navbar.Collapse>
-  //           <Form className="d-flex">
-  //               <FormControl
-  //                 type="search"
-  //                 placeholder="Search Movie"
-  //                 className="me-2"
-  //                 aria-label="Search"
-  //               />
-  //              <Button className='button'>Search</Button>
-  //             </Form>
-  //         </Container>
-  //       </Navbar>
-
-  //       {selectedMovie
-  //         ? (
-  //           <Row className='justify-content-md-center'>
-  //             <Col md={8}>
-  //               <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-  //             </Col>
-  //           </Row>
-  //           )
-  //         : (
-  //           <Row className="justify-content-md-center">
-  //             <Col md={3}>
-  //             {movies.map(movie => (
-  //               <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-  //             ))}
-  //             </Col>
-  //           </Row>
-  //         )
-  //       }
-  //     </div>
-  //   );
-  // }
 }
 
