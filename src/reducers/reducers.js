a2652d54ec9884525d5ae675d355a44960db5610
Mr-Macko <max.allesch@gmx.de>
2022-02-02 00:13:20 +0100

    changed combined reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -22,12 +22,10 @@ function movies(state = [], action) {
 }
 
 
-function moviesApp(state = {}, action) {
-    return {
-        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-        movies: movies(state.movies, action)
-    }
-}
+const moviesApp = combineReducers({
+    visibilityFilter,
+    movies
+  });
 
 
 export default moviesApp;
